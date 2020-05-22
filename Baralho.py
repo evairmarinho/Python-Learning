2daca63b0d6d18a13f3da7804e6582b9001378e5
Evair Marinho <evairmarinho@hotmail.com>
2020-05-22 01:37:16 -0300

    correções

--- Baralho.py
+++ Baralho.py
@@ -1,10 +1,12 @@
-naipes= ["Espadas","Copas","Ouros","Paus"]
-simbolos = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+naipes = ["Espadas","Copas","Ouros","Paus"]
+simbolos = ["1","2","3","4","5","6","7","8","9","10","A","Rei","Rainhas","Valete"]
+cartas=[' '],[' ']
 k=0
 l=0
 
 for i in naipes:
     for j in simbolos:
-        print(""+naipes[k]+" "+simbolos[l])
-        l+1
-k+1
+       cartas[k][l]=i+" "+j
+       print(cartas[k][l])
+       l+1
+k+1 
